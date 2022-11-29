531e8568dc9110c08f4a4d506760daefe82623a9
sam1016yu <miaoyu1016@gmail.com>
2022-11-28 21:23:43 -0500

    modify run script

--- mysql-exp/tpcc_runall.sh
+++ mysql-exp/tpcc_runall.sh
@@ -15,6 +15,14 @@ run_one (){
 	num_WH=$2
 	num_chunk=$3
 	chunk_size=$4
+	result_dir=~/results_tpcc/WH@${num_WH}~T@${num_terminal}~num_chunk@${num_chunk}~chunk_size@${chunk_size}
+	if [[ -d "$result_dir" ]];then
+		echo "Already runned setting WH@${num_WH}~T@${num_terminal}~num_chunk@${num_chunk}~chunk_size@${chunk_size}"
+		echo "===================================================="
+		return 0
+	fi
+	#clean up binary log
+	ssh miaoyu@server 'sudo mysql -se "PURGE BINARY LOGS BEFORE NOW()";'
 	set_buffer_size $buffer_pool $num_chunk $chunk_size
 	#modify config file
 	~/benchbase/mysql-exp/config_tpcc.py $num_terminal $num_WH
@@ -24,65 +32,17 @@ run_one (){
 	java -jar benchbase.jar -b tpcc \
 	-c ~/benchbase/mysql-exp/sample_tpcc_config.xml \
 	--create=true --load=true --execute=true \
-	-d ~/results_tpcc/WH@${num_WH}~T@${num_terminal}~num_chunk@${num_chunk}~chunk_size@${chunk_size}
-	#clean up binary log
-	ssh miaoyu@server 'sudo mysql -se "PURGE BINARY LOGS BEFORE NOW()";'
+	-d $result_dir
 }
 
 
-# for WH in 16 32
-# do
-# 	for terminal in 100 120 130 140 145
-# 	do
-# 		for num_chunk in 1 2 4 8
-# 		do
-# 			for chunk_size in 1024 2048 4096
-# 			do	
-# 				run_one $terminal $WH $num_chunk $chunk_size
-# 				echo "************************"
-# 			done
-# 		done
-# 	done
-# done
-
-# for WH in 2 4 8
-# do
-# 	for terminal in 1 2 4 8 16 32 64 128 130 140 145
-# 	do
-# 		for num_chunk in 1 2 4 8
-# 		do
-# 			for chunk_size in 1024 2048 4096
-# 			do	
-# 				run_one $terminal $WH $num_chunk $chunk_size
-# 				echo "************************"
-# 			done
-# 		done
-# 	done
-# done
-
-
-# for WH in 16 32
-# do
-# 	for terminal in 1 2 4 8 16 32 64 128
-# 	do
-# 		for num_chunk in 1 2 4 8
-# 		do
-# 			for chunk_size in 1024 2048 4096
-# 			do	
-# 				run_one $terminal $WH $num_chunk $chunk_size
-# 				echo "************************"
-# 			done
-# 		done
-# 	done
-# done
-
-for WH in 64
+for WH in 1 2 4 8 16 32 64 128
 do
-	for terminal in 140 145
+	for terminal in 1 2 4 8 16 32 64 128 256 512
 	do
 		for num_chunk in 1 2 4 8
 		do
-			for chunk_size in 1024 2048 4096
+			for chunk_size in 1024 2048 4096 8192
 			do	
 				run_one $terminal $WH $num_chunk $chunk_size
 				echo "************************"
@@ -90,6 +50,4 @@ do
 		done
 	done
 done
-
-# run_one 145 10 10 1024
 
