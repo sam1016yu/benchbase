2a75549b8d66dda0fdafcb50520e3258e2dd5deb
sam1016yu <miaoyu1016@gmail.com>
2023-01-06 16:56:28 -0500

    new config

--- mysql-exp/config_tpcc.py
+++ mysql-exp/config_tpcc.py
@@ -4,9 +4,11 @@ import xml.etree.ElementTree as ET
 import sys
 
 
-if len(sys.argv) < 3:
-    print("Input:<numTerminals> <numScaleFactor>")
+if len(sys.argv) < 4:
+    print("Input:<numTerminals> <numScaleFactor> <server id>")
+    exit(1)
     
+server_ip = "jdbc:mysql://10.10.1.{}:3306/benchbase?rewriteBatchedStatements=true".format(sys.argv[3])
 fpath = "/users/miaoyu/benchbase/mysql-exp/sample_tpcc_config.xml"
 
 tree = ET.parse(fpath)
@@ -14,5 +16,6 @@ root = tree.getroot()
 
 root.find("terminals").text = sys.argv[1]
 root.find("scalefactor").text = sys.argv[2]
+root.find("url").text = server_ip
 
 tree.write(fpath)
---
+++ mysql-exp/tpcc_ram.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+
+# set_buffer_size (){
+# 	num_chunk=$1
+# 	chunk_size=$2
+# 	scp ~/benchbase/mysql-exp/set-buffer.sh miaoyu@server:~
+#     # set mysql bufffer pool size, both number of chunks and size of each chunk
+# 	ssh miaoyu@server "sudo ~/set-buffer.sh  $num_chunk $chunk_size"
+# 	sleep 2
+# }
+# ssh miaoyu@server 'sudo mysql -se "PURGE BINARY LOGS BEFORE NOW()";'
+
+
+if (($# < 7))
+then
+    echo "missing arguments"
+    exit 1
+fi
+
+num_terminal=$1
+num_WH=$2
+num_chunk=$3
+chunk_size=$4
+num_log=$5
+log_size=$6
+server_id=$7
+
+
+result_dir=~/results_tpcc/WH@${num_WH}~T@${num_terminal}~num_chunk@${num_chunk}~chunk_size@${chunk_size}~num_log@${num_log}~log_size@${log_size}
+#modify config file
+~/benchbase/mysql-exp/config_tpcc.py $num_terminal $num_WH $server_id
+cd ~/benchbase/target/benchbase-mysql
+#launch benchbase
+java -jar benchbase.jar -b tpcc \
+-c ~/benchbase/mysql-exp/sample_tpcc_config.xml \
+--create=true --load=true --execute=true \
+-d $result_dir
+#clean raw result
+rm  $result_dir/*.raw.csv
+
